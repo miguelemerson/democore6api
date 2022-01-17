0977966b42f87742dcb9e1d8ea587357e219a630
miguelemerson <mperedo@gmail.com>
2022-01-16 22:27:47 -0300

    Update WeatherForecastController.cs

--- DemoCore6API/DemoCore6API/Controllers/WeatherForecastController.cs
+++ DemoCore6API/DemoCore6API/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace DemoCore6API.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[0] //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
